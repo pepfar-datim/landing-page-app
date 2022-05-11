80ce77b4672c10fcaea61f0b9c4869b685d56d3c
Jakub Flaska <jflaska@baosystems.com>
2022-05-11 15:16:07 -0400

    new base url service

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import datimApi,{getBaseUrl} from "@pepfar-react-lib/datim-api"
+import {Main} from './main.component';
+import datimApi from "@pepfar-react-lib/datim-api"
 import {Provider} from "@dhis2/app-runtime";
-import {HeaderBar} from '@dhis2/ui-widgets'
-
-datimApi.registerProd(getBaseUrl())
+// @ts-ignore
+import {HeaderBar} from "@dhis2-ui/header-bar";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+console.log(process.env)
+
+datimApi.register(process.env.NODE_ENV,process.env.REACT_APP_BASE_URL);
+
 root.render(
   <React.StrictMode>
-      <Provider config={{baseUrl: getBaseUrl(), apiVersion: 36}}>
+      <Provider config={{baseUrl: datimApi.getBaseUrl(), apiVersion: 36}}>
             <span id='dhis2HeaderBar'>
                 <HeaderBar/>
             </span>
-    <App />
+            <Main/>
       </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
