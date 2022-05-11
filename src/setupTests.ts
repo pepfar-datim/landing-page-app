d35630593bc6d06a7f9031915692e7af4a7e0c29
Jakub Flaska <jflaska@baosystems.com>
2022-05-11 15:40:59 -0400

    -a -m setting up jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import datimApi from "@pepfar-react-lib/datim-api"
+
+datimApi.register(process.env.NODE_ENV, process.env.REACT_APP_BASE_URL)
