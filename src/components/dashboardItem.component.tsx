edcd17fca932afc74f8a6f6b1bc3eac09613b470
Jakub Flaska <jflaska@baosystems.com>
2022-05-17 14:59:32 -0400

    resize working

--- src/components/dashboardItem.component.tsx
+++ src/components/dashboardItem.component.tsx
@@ -1,14 +1,29 @@
 import {DashboardItem} from "../types/dashboard.type";
-import {getWidth} from "../services/getWidthHeight.service";
-import React from "react";
-import {Paper} from "@mui/material"
+import {getColumnSize, getWidth} from "../services/getWidthHeight.service";
+import React, {RefObject, useEffect, useRef} from "react";
+import {Grid, Paper} from "@mui/material"
 import "./css/dashboardItem.component.css"
 
+function resizeObject(ref:RefObject<HTMLObjectElement>){
+    let height:number = ref.current?.contentDocument?.querySelector('html')?.offsetHeight as number;
+    if (height && height>160 && ref.current) ref.current.style.height = `${height}px`;
+}
+
+function setupResize(ref:RefObject<HTMLObjectElement>){
+    window.addEventListener('resize',()=>resizeObject(ref));
+}
+
 export function DashboardItemComponent({dashboardItem}:{dashboardItem:DashboardItem}){
-    return <Paper className={'Paper'}><object
-        width={getWidth(dashboardItem.width)}
-        height={getWidth(dashboardItem.height)}
-        data={`/api/apps/Information/index.html?dashboardItemId=${dashboardItem.id}#/`}
-        type={'text/html'}
-    /></Paper>
+    const objectRef=useRef<HTMLObjectElement>(null);
+    useEffect(()=>setupResize(objectRef));
+    return <Grid item md={getColumnSize(dashboardItem.width)} xs={12}>
+        <Paper className={'Paper'}>
+            <object
+                ref={objectRef}
+                data={`/api/apps/Information/index.html?dashboardItemId=${dashboardItem.id}#/`}
+                type={'text/html'}
+            />
+            {/*<iframe src={`/api/apps/Information/index.html?dashboardItemId=${dashboardItem.id}#/`}/>*/}
+        </Paper>
+    </Grid>
 }
--- src/services/getWidthHeight.service.ts
+++ src/services/getWidthHeight.service.ts
@@ -1,3 +1,7 @@
 export function getWidth(originalDimension:number){
     return originalDimension/61 * window.innerWidth;
 }
+
+export function getColumnSize(originalDimension:number):number{
+    return originalDimension/60 * 12;
+}
