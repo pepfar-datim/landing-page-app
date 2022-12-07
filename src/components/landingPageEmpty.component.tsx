f1362b0a3afa7810d5aa8ab9e34e9ba094d7bcc0
Peter Linnegan <plinnegan@baosystems.com>
2022-12-07 10:27:48 +0000

    fix: create empty dashboard if needed and add blank component

---
+++ src/components/landingPageEmpty.component.tsx
@@ -0,0 +1,9 @@
+import './css/landingPageEmpty.component.css'
+
+export const LandingPageEmpty = () => (
+  <div className="emptyLandingPage">
+    No items to display
+  </div>
+);
+
+export default LandingPageEmpty;
--- src/components/main.component.tsx
+++ src/components/main.component.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import {getDashboardItems} from "../services/getDashboardItems.service";
 import {DashboardItem} from "../types/dashboard.type";
 import { DashboardItemComponent } from './dashboardItem.component';
+import LandingPageEmpty from './landingPageEmpty.component';
 import {CircularProgress, Grid} from "@mui/material";
 
-export class Main extends React.Component<any, {dashboardItems:DashboardItem[]}>{
+export class Main extends React.Component<any, {loaded: boolean, dashboardItems:DashboardItem[]}>{
     constructor(props:any) {
         super(props);
-        this.state = {dashboardItems:[]};
+        this.state = {
+            loaded: false,
+            dashboardItems: []
+        };
     }
     componentDidMount() {
-        getDashboardItems('LandingPage').then((dashboardItems:DashboardItem[])=>this.setState({dashboardItems}));
+        getDashboardItems('LandingPage').then((dashboardItems:DashboardItem[])=>{
+            this.setState({
+                loaded: true,
+                dashboardItems
+            })
+        });
     }
 
     render() {
-        if (this.state.dashboardItems.length===0) return <CircularProgress/>
+        if (!this.state.loaded) return <CircularProgress/>
+        if (this.state.loaded && this.state.dashboardItems.length === 0) return <LandingPageEmpty />
         return <div id={'gridWrapper'}><Grid container spacing={1}>
             {this.state.dashboardItems.map((dashboardItem:DashboardItem,i:number)=><DashboardItemComponent dashboardItem={dashboardItem} key={i}/>)}
         </Grid></div>
