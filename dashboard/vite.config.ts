4fcc1b2bf7d3ca587c417060118cb3fc9d45a9d5
Jakub Flaska <jflaska@baosystems.com>
2024-03-21 12:20:02 -0400

    working

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -6,16 +6,15 @@ try {
     //@ts-expect-error process not defined
     auth = loadEnv('development', process.cwd())['VITE_DHIS_AUTH']
 } catch (e) {
-    //@ts-expect-error error not defined
     throw new Error(`You have to specify VITE_DHIS_AUTH env variable. E.g. btoa('username:password') in JavaScript`)
 }
 
 
 
 const proxy:ProxyOptions = {
-    target: 'https://dev.datim.org/',
+    // target: 'https://dev.datim.org/',
+    target: 'https://ben.datim.org/',
     // target: 'https://nr.testing.datim.org/',
-    //@ts-expect-error proxy not used
     configure: (proxy, options) => {
         options.headers = {Authorization: `Basic ${auth}`}
     },
