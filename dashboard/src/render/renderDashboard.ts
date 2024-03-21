5815064d5ff5e60dd6014c93ac9701d685391710
Jakub Flaska <jflaska@baosystems.com>
2024-03-21 12:37:29 -0400

    fixed incorrect width

--- dashboard/src/render/renderDashboard.ts
+++ dashboard/src/render/renderDashboard.ts
@@ -1,21 +1,33 @@
-import {Dashboard, Widget} from "../types/dashboard.types.ts";
-import {dashboardStyles, objectStyles, rowStyles} from "./render.styles.ts";
+import {Dashboard, Widget} from "../types/dashboard.types.ts"
+import './render.styles.css'
 
 function getMinHeight(){
     return 640000/window.innerWidth/2
+}
+
+function getObjectWidth(width:number):number{
+    return width/59*100
+}
+
+function getObjectDimensions(width:number):string{
+    return `
+        min-height: ${getMinHeight()}px;
+        width: ${getObjectWidth(width)}%;
+    `
 }
 
 function renderWidget({id, size:{width}}:Widget):string{
     return `<object
         id="${id}"
         data="/api/apps/Information/index.html?dashboardItemId=${id}#/"
-        style="${objectStyles};flex-grow:${width};min-height: ${getMinHeight()}px;"
+        style="${getObjectDimensions(width)}"
         onload="initializeContainerResize('${id}')"
+        class="object"
     ></object>`
 }
 
 function renderRow(widgets:Widget[]):string{
-    return `<div style="${rowStyles}">
+    return `<div class="row">
         ${widgets.map(renderWidget).join('')}
     </div>`
 }
@@ -26,7 +38,7 @@ function cleanHtml(html:string):string{
         .replace(/  +/g,' ')
 }
 export function renderDashboard({rows}:Dashboard){
-    const html:string = cleanHtml(`<div style="${dashboardStyles}">
+    const html:string = cleanHtml(`<div id='dashboard'>
         ${rows.map(renderRow).join('')}
     </div>`)
     document.getElementById('content')!.innerHTML = html
