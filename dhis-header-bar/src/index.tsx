70d45c1373432e9bb7a9c585e2a9ffe2f5fed504
Pete Linnegan <plinnegan@baosystems.com>
2024-10-03 12:30:55 +0100

    fix: dependency issues with the header bar module

--- dhis-header-bar/src/index.tsx
+++ dhis-header-bar/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import Dhis2HeaderBar from "./headerBar.component.tsx";
 
-ReactDOM.createRoot(document.getElementById('dhis-header-bar')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <Dhis2HeaderBar/>
   </React.StrictMode>,
+  document.getElementById('dhis-header-bar')!
 )
